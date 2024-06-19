e0af127ede6c0c41af237729acf487aafd508eed
Samson Inakoju <samminakoju@gmail.com>
2024-06-19 07:59:15 +0100

    added drq.py to act as the entrypoint of the click app

---
+++ drq.py
@@ -0,0 +1,21 @@
+import click
+import commands
+
+
+@click.group()
+@click.version_option("0.1.0", prog_name="drq")
+def main():
+    """
+    This cli tool is used to get random quotes
+    """
+    pass
+
+
+main.add_command(commands.get_random)
+main.add_command(commands.list_all)
+main.add_command(commands.add_new)
+
+
+if __name__ == "__main__":
+    main()
+
